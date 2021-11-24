bd93894f0103786aa1e8f9a260cb63b998f2c198
snakifyanswers <amysammyluke@gmail.com>
2021-11-24 19:50:28 +0000

    MODULE 2 BABYYYYY

--- js/main.js
+++ js/main.js
@@ -47,8 +47,17 @@ function createModule(num){
                 createLesson(lessonSelector, "School desks", "c0238adf693a63a5ba77dd4c9966c5f7", 8);
                 break;
             case 2:
-                createLesson(lessonSelector, "Example 2","bd10e846c6eae4419c3041f3da1b82a0",0);
-                break;
+                createLesson(lessonSelector, "Last digit of integer","d41c3de0d5e589bbdb69df70b0e191a9",0);
+                createLesson(lessonSelector, "Tens digit", "b5f09ac0f93f88c452ff98071100560f",1);
+                createLesson(lessonSelector, "Sum of digits", "bcaf27c55d150eb1965c03963dd64a88", 2);
+                createLesson(lessonSelector, "Fractional Part", "095150ef3e177f5d351f43cf7a91f47e", 3);
+                createLesson(lessonSelector, "First digit after decimal point", "d2b75ba8f545eb7850f224db08808ce7", 4);
+                createLesson(lessonSelector, "Car route", "b2344f3630604234b87c5eed77a18ce3", 5);
+                createLesson(lessonSelector, "Digital Clock", "9c5390656207c3d88dafdb1417b683c4", 6);
+                createLesson(lessonSelector, "Total cost", "48e783127e8645215dd0e1018bbf278e", 7);
+                createLesson(lessonSelector, "Clock face - 1", "256dddcba28362aa2a3307791cf2a8b3", 8);
+                createLesson(lessonSelector, "Clock face - 2", "8b888e4833bee0828e3b00040b8c8c9f");
+                break; 
             }
 
 
