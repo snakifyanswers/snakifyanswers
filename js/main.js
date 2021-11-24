c32c0c7fc53b7c364acecaf005bfc33f489b0503
snakifyanswers <amysammyluke@gmail.com>
2021-11-24 17:36:55 +0000

    ok

--- js/main.js
+++ js/main.js
@@ -1,36 +1,36 @@
 var isDisplaying = false
 var currentModule = 0;
-var module1 = document.getElementById("module1");
 
 function createModule1(){
-    console.info("create module");
-    //var lessonSelector = document.getElementById("lesson-selector")
-    //lessonSelector.remove();
     if(isDisplaying){
         var lessonSelector = document.getElementById("lesson-selector");
         lessonSelector.remove();
         isDisplaying = false;
         currentModule = 0;
     } else{
-        var main = document.getElementById("main");
-
         var lessonSelector = document.createElement("div");
         lessonSelector.classList.add("lesson-selector")
         lessonSelector.id = "lesson-selector";
 
-        for (let i = 0; i < 9; i++) {
-            var lessonDiv = document.createElement("div");
-            lessonDiv.id = "lesson1";
-            lessonDiv.innerText = i + 1;
-            lessonSelector.appendChild(lessonDiv);
-        }
-
+        createLesson(lessonSelector, "Sum of three numbers", "2d526d30bfdaddc3b1853f96456e0947")
+        var main = document.getElementById("main");
         main.appendChild(lessonSelector);
         isDisplaying = true;
         currentModule = 1;
     }
 }
 
-function openScriptPopup(url){
-    
+function createLesson(lessonSelector, text, gistId){
+    var lessonDiv = document.createElement("div");
+    lessonDiv.innerText = text;
+    lessonDiv.onclick = function(){
+        createGist(gistId);
+    }
+    lessonSelector.appendChild(lessonDiv);
+}
+
+function createGist(gistId = "bd10e846c6eae4419c3041f3da1b82a0"){
+    var script = document.createElement("script");
+    script.src = "https://gist.github.com/snakifyanswers/" + gistId + ".js";
+    document.body.appendChild(script)    
 }
